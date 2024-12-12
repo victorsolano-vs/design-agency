6fb878488026039e25f9525787ddb2e6add8d92e
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-12 10:14:07 -0400

    testing hover-effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+import HoverEffect from 'hover-effect'
+import img1 from './images/Img10.jpg'
+import img2 from './images/Img14.jpg'
+import displacement from './images/7.jpg'
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize HoverEffect
+    new HoverEffect({
+      parent: containerRef.current, // Container where the effect will be applied
+      intensity: 0.9, // Adjust intensity of the effect
+      image1: img1, // Path to the first image
+      image2: img2, // Path to the second image
+      displacementImage: displacement, // Path to the displacement image
+    });
+  }, []);
+
+
+
 
   return (
     <>
@@ -27,6 +49,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div
+      ref={containerRef}
+      style={{
+        width: "500px",
+        height: "300px",
+        overflow: "hidden"
+      }}
+    ></div>
     </>
   )
 }
